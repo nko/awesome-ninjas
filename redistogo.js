8c56e0bda79f51597f90103bbca01e2a21ba201d
Anil Wadghule <anildigital@gmail.com>
2010-08-28 17:58:38 +0530

    Basic redistogo setup done.

---
+++ redistogo.js
@@ -0,0 +1,15 @@
+var sys = require('sys');
+var redis = require("./deps/redis-node-client/lib/redis-client");
+var db = redis.createClient(9272, 'goosefish.redistogo.com');
+var dbAuth = function() { db.auth('399b9fcf4cbd80b7194fe76bb70e00fc'); }
+db.addListener('connected', dbAuth);
+db.addListener('reconnected', dbAuth);
+
+dbAuth();
+
+
+db.info(function (err, info) {
+    if (err) throw new Error(err);
+    sys.puts("Redis Version is: " + info.redis_version);
+    db.close();
+});
