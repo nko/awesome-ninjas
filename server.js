ca908f6779c05c5f7d6d31e97d2796eb88c308e9
Arnab Chakraborty <arnabc212@gmail.com>
2010-08-28 16:00:13 +0530

    Added changes for twitter message polling

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Fetch feed module
 
 
-var sys= require('sys')
+/*var sys= require('sys')
 
 var OAuth= require('./deps/node-oauth/lib/oauth').OAuth;
 
@@ -17,8 +17,22 @@ var oa= new OAuth("https://api.twitter.com/oauth/request_token",
 oa.get("http://api.twitter.com/1/statuses/mentions.json", "181109840-v81EANFF7BQME1B3thPE43iHLjs9PY3Z7ywdZes8", "1NzWBrOFJ4Jk0ipXvSMtkYh8hVAKEpOC5whgZ3BPSSY", function(error, data, response) {
 
     sys.puts(sys.inspect(data));
-});
+});*/
 
+
+var TwitterPoll = require('./lib/poll' );
+
+var tpoll = new TwitterPoll();
+
+tpoll.on( 'mentions', function ( data ) {
+    console.log( data );    
+} );
+
+tpoll.on( 'directMessages', function ( data ) {
+    console.log( data );
+} );
+
+tpoll.directMessages().mentions();
 
 // Parse feed module
 
