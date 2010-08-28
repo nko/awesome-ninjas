592942f0a01ba067549d9cb80462f5a0b5ee0017
Anil Wadghule <anildigital@gmail.com>
2010-08-28 17:18:42 +0530

    Updates
    Added Parser for parising twitter messages

--- server.js
+++ server.js
@@ -1,31 +1,26 @@
 // Fetch feed module
-
-
-/*var sys= require('sys')
-
-var OAuth= require('./deps/node-oauth/lib/oauth').OAuth;
-
-var oa= new OAuth("https://api.twitter.com/oauth/request_token",
-                  "https://api.twitterx`.com/oauth/access_token",
-                  "Qyh75skaVfaUIjRNYxluw",
-                  "rUnr1gvHXLUlHcHvFbieazIO1wApF291eZGudMKBM",
-                  "1.0",
-                  null,
-                  "HMAC-SHA1")
-
-
-oa.get("http://api.twitter.com/1/statuses/mentions.json", "181109840-v81EANFF7BQME1B3thPE43iHLjs9PY3Z7ywdZes8", "1NzWBrOFJ4Jk0ipXvSMtkYh8hVAKEpOC5whgZ3BPSSY", function(error, data, response) {
-
-    sys.puts(sys.inspect(data));
-});*/
-
-
-var TwitterPoll = require('./lib/poll' );
+var TwitterPoll = require('./lib/twitter' );
 
 var tpoll = new TwitterPoll();
 
+var CommandProcessor = require('./lib/command_processor');
+
 tpoll.on( 'mentions', function ( data ) {
-    console.log( data );    
+    console.log( typeof(data) );
+    
+    var data = JSON.parse(data),
+    cmdMap;
+
+    if (Array.isArray(data)) 
+        cmdMap = data.map(function (v, index, arr) {
+            return Parser(v);
+        });
+        
+    
+    setTimeout(function() {
+        CommandProcesor.process(cmdMap);
+    }, 0);
+    
 } );
 
 tpoll.on( 'directMessages', function ( data ) {
@@ -33,6 +28,8 @@ tpoll.on( 'directMessages', function ( data ) {
 } );
 
 tpoll.directMessages().mentions();
+
+
 
 // Parse feed module
 
