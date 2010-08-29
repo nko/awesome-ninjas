052b4029a02f6b41b6d7765991bab3c6f851c0ae
Anil Wadghule <anildigital@gmail.com>
2010-08-29 14:40:04 +0530

    Updates for mention_since_id

--- init_message_poll.js
+++ init_message_poll.js
@@ -1,28 +1,33 @@
 var Twitter          = require('./lib/twitter' ),
-    CommandProcessor = require('./lib/command_processor'),
-    CommandParser    = require('./lib/parser'),
+CommandProcessor = require('./lib/command_processor'),
+CommandParser    = require('./lib/parser'),
+Redis = require('./lib/redis'),
 
-    // twitter communication instance
-    T                = new Twitter(),
-    timeout;
+// twitter communication instance
+T                = new Twitter(),
+timeout,
+db;
 
 
 // twitter message callback
 // parses the JSON encoded messages and then converts them
 // to commands to executed by CommandProcessor
 function twitterCallback( data ) {
-    var map;
+    var map=[], l, r;
 
-    if( Array.isArray( data ) && data )
-        map = data.map( function ( tweet ) {
-            return CommandParser.parse( tweet );
-        } );
-
+    if( Array.isArray( data ) && data ) {
+        l = data.length;
+        while(--l >= 0) {
+            r = CommandParser.parse( data[l] );
+            if( r )
+                map.push( r );
+        }
+    }
     console.log( map );
 
     // execute asynchronously
-    setTimeout( CommandProcessor.process, 0, map, function ( ob ) {
-        T.updateStatus( ob );
+    setTimeout( CommandProcessor.process, 0, map, function (ob) {
+        T.updateStatus(ob);
     } );
 }
 
@@ -30,11 +35,16 @@ function twitterCallback( data ) {
 // subscribe to Twitter message events
 T.on( 'mentions', function ( data ) {
     console.log('Mention Message received %s', new Date() );
+    T.setMentionSinceId(data[0].id);
     twitterCallback( data );
 } );
-T.on( 'directMessages', function ( data ) {
-    console.log('Direct Message received %s', new Date() );
-    twitterCallback( data );
+
+T.on( 'updateStatus', function ( data ) {
+    db = Redis.getDb();
+    console.log(data);
+    db.set( 'mention_since_id', data[0].id, function() {
+        db.close();
+    });
 } );
 
 T.on( 'error', function (err, res) {
@@ -43,13 +53,17 @@ T.on( 'error', function (err, res) {
 } );
 
 
-function startFetch () {
-    // fetch messages
-    T.mentions().directMessages();    
-}
+db = Redis.getDb();
+db.get("mention_since_id", function(err, val){
+    T.setMentionSinceId(val);
+    // start polling
+    timeout = setInterval(function() {
+        T.mentions();    
+    }, 5000 );
 
-// start polling
-timeout = setInterval( startFetch, 5000 );
+    db.close();
+});
+
 
 process.on( 'exit', function () {
     clearInterval( timeout );
